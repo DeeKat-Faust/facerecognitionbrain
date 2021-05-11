4e54aa85e7c798cee3b3b0bb33f4dc4e7d2c0762
DeeKat <deepak.281295@gmail.com>
2021-05-12 02:11:55 +0530

    updated code to handle cors policy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import dotenv from 'dotenv';
-import './App.css';
-import Particles from 'react-particles-js';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
+import React from "react";
+import dotenv from "dotenv";
+import "./App.css";
+import Particles from "react-particles-js";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 
 dotenv.config();
 
@@ -18,29 +18,28 @@ const particleOptions = {
       value: 80,
       density: {
         enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+        value_area: 800,
+      },
+    },
+  },
+};
 
 const initialState = {
-  input: '',
-  imageUrl: '',
+  input: "",
+  imageUrl: "",
   boxLocations: [],
-  route: 'signin',
+  route: "signin",
   isSignedIn: false,
   user: {
-    id: '',
-    email: '',
-    name: '',
-    entries: '',
-    joined: ''
-  }
-}
+    id: "",
+    email: "",
+    name: "",
+    entries: "",
+    joined: "",
+  },
+};
 
 class App extends React.Component {
-
   constructor() {
     super();
     this.state = initialState;
@@ -50,69 +49,79 @@ class App extends React.Component {
     // fetch('https://smartbrainfacedetector-api.herokuapp.com/')
     //   .then(response => response.json())
     //   .then(console.log)
+
+    fetch("https://smartbrainfacedetector-api.herokuapp.com/")
+      .then((response) => response.json())
+      .then(console.log);
   }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
-  }
+  };
 
   onPictureSubmit = (event) => {
     this.setState({ imageUrl: this.state.input });
 
-    fetch('https://smartbrainfacedetector-api.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("https://smartbrainfacedetector-api.herokuapp.com/imageurl", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify({
-        input: this.state.input
-      })
-    }).then(response => response.json())
-      .then(response => {
-        fetch('https://smartbrainfacedetector-api.herokuapp.com/image', {
-          method: 'put',
-          headers: { 'Content-Type': 'application/json' },
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        fetch("https://smartbrainfacedetector-api.herokuapp.com/image", {
+          method: "put",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
           body: JSON.stringify({
-            email: this.state.user.email
-          })
-        }).then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
-          })
-        if (response !== 'API is unavailable') {
+            email: this.state.user.email,
+          }),
+        })
+          .then((response) => response.json())
+          .then((count) => {
+            this.setState(Object.assign(this.state.user, { entries: count }));
+          });
+        if (response !== "API is unavailable") {
           this.displayFaceBox(this.calculateFaceLocation(response));
         }
       });
-  }
+  };
 
   calculateFaceLocation = (data) => {
-    const image = document.getElementById('inputImage');
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
 
-    const regions = data['outputs'][0]['data']['regions'];
+    const regions = data["outputs"][0]["data"]["regions"];
     const boxLocations = regions.map((region) => {
-      let boundingBox = region['region_info']['bounding_box'];
+      let boundingBox = region["region_info"]["bounding_box"];
       return {
         top: height * boundingBox.top_row,
-        right: width - (width * boundingBox.right_col),
-        bottom: height - (height * boundingBox.bottom_row),
+        right: width - width * boundingBox.right_col,
+        bottom: height - height * boundingBox.bottom_row,
         left: width * boundingBox.left_col,
-      }
+      };
     });
 
     return boxLocations;
-  }
+  };
 
   displayFaceBox = (boxLocations) => {
     this.setState({ boxLocations: boxLocations });
-  }
+  };
 
   onRouteChange = (route) => {
-    if (route === 'home')
-      this.setState({ isSignedIn: true });
-    else
-      this.setState(initialState);
+    if (route === "home") this.setState({ isSignedIn: true });
+    else this.setState(initialState);
     this.setState({ route: route });
-  }
+  };
 
   loadUser = (data) => {
     this.setState({
@@ -121,31 +130,41 @@ class App extends React.Component {
         email: data.email,
         name: data.name,
         entries: data.entries,
-        joined: data.joined
-      }
-    })
-  }
+        joined: data.joined,
+      },
+    });
+  };
 
   render() {
     const { isSignedIn, route, imageUrl, boxLocations } = this.state;
     return (
       <div className="App">
-        <Particles className='particles' params={particleOptions} />
-        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
-        {
-          route === 'signin'
-            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            : (
-              route === 'register'
-                ? <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-                : <>
-                  <Logo />
-                  <Rank name={this.state.user.name} entries={this.state.user.entries} />
-                  <ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit} />
-                  <FaceRecognition imageUrl={imageUrl} boxLocations={boxLocations} />
-                </>
-            )
-        }
+        <Particles className="particles" params={particleOptions} />
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}
+        />
+        {route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : route === "register" ? (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        ) : (
+          <>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onPictureSubmit={this.onPictureSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} boxLocations={boxLocations} />
+          </>
+        )}
       </div>
     );
   }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,120 +1,134 @@
-import React from 'react';
+import React from "react";
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      name: "",
+      password: "",
+      error: "",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            name: '',
-            password: '',
-            error: ''
-        }
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  onRegisterSubmit = (event) => {
+    event.preventDefault();
+
+    if (!this.state.email.length) {
+      this.setState({ error: "Email field is empty" });
+    } else if (!this.state.name.length) {
+      this.setState({ error: "Name field is empty" });
+    } else if (this.state.password.length < 8) {
+      this.setState({
+        error: "Password must be atleast 8 characters in length",
+      });
+    } else {
+      fetch("https://smartbrainfacedetector-api.herokuapp.com/register", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          email: this.state.email.toLowerCase(),
+          name: this.state.name,
+          password: this.state.password,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data === "Email is already in use") {
+            this.setState({ error: data });
+          } else if (data === "Error registering user!") {
+            this.setState({ error: data });
+          } else {
+            this.setState({ error: "" });
+            this.props.loadUser(data);
+            this.props.onRouteChange("home");
+          }
+        });
     }
+  };
 
-    onEmailChange = (event) => {
-        this.setState({ email: event.target.value });
-    }
-
-    onNameChange = (event) => {
-        this.setState({ name: event.target.value });
-    }
-
-    onPasswordChange = (event) => {
-        this.setState({ password: event.target.value });
-    }
-
-    onRegisterSubmit = (event) => {
-        event.preventDefault();
-
-        if (!this.state.email.length) {
-            this.setState({ error: 'Email field is empty' });
-        } else if (!this.state.name.length) {
-            this.setState({ error: 'Name field is empty' });
-        } else if (this.state.password.length < 8) {
-            this.setState({ error: 'Password must be atleast 8 characters in length' });
-        } else {
-            fetch('https://smartbrainfacedetector-api.herokuapp.com/register', {
-                method: 'post',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    email: this.state.email.toLowerCase(),
-                    name: this.state.name,
-                    password: this.state.password
-                })
-            }).then(response => response.json())
-                .then(data => {
-                    if (data === 'Email is already in use') {
-                        this.setState({ error: data });
-                    } else if (data === 'Error registering user!') {
-                        this.setState({ error: data });
-                    } else {
-                        this.setState({ error: '' });
-                        this.props.loadUser(data);
-                        this.props.onRouteChange('home');
-                    }
-                });
-        }
-    }
-
-    render() {
-
-        return (
-            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-                <main className="pa4 black-80">
-                    <form className="measure">
-                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 fw6 ph0 mh0">Register</legend>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input 
-                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                    type="email" 
-                                    name="email-address" 
-                                    id="email-address" 
-                                    onChange={this.onEmailChange}
-                                />
-                            </div>
-                            <div className="mt3">
-                                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                                <input 
-                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                    type="text" 
-                                    name="name" 
-                                    id="name" 
-                                    onChange={this.onNameChange}
-                                />
-                            </div>
-                            <div className="mv3">
-                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input 
-                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                    type="password" 
-                                    name="password" 
-                                    id="password" 
-                                    onChange={this.onPasswordChange}
-                                />
-                            </div>
-                        </fieldset>
-                        <div className="">
-                            <input
-                                onClick={this.onRegisterSubmit}
-                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                                type="submit"
-                                value="Register"
-                            />
-                        </div>
-                        {   this.state.error.length
-                            ? <div className="w-90 ba br2 pa3 ma2 red bg-washed-red" role="alert">
-                                {this.state.error}
-                            </div>
-                            : <></>
-                        }
-                    </form>
-                </main>
-            </article>
-        );
-    }
+  render() {
+    return (
+      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <form className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onRegisterSubmit}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+              />
+            </div>
+            {this.state.error.length ? (
+              <div
+                className="w-90 ba br2 pa3 ma2 red bg-washed-red"
+                role="alert"
+              >
+                {this.state.error}
+              </div>
+            ) : (
+              <></>
+            )}
+          </form>
+        </main>
+      </article>
+    );
+  }
 }
 
 export default Register;
